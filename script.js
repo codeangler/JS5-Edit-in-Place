21895015c4c0bd9d221f21cb7aaec16c951309e8
Casey Burnett <burnett.casey@gmail.com>
2016-05-24 14:58:24 -0600

    access to DOM through event listener

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// set up test passed
+var nameData = document.querySelectorAll('.textInput');
+	console.log(nameData);
+
+for (var i = 0; i < nameData.length; i ++){
+	nameData[i].addEventListener("click", alertText, false);
+		
+}
+
+
+function alertText(){
+	alert("you did it, again." + " You have clicked item " + [i])}
