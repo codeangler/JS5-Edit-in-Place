dc678dfceb0ba1be2b4cc5baa380898d063e9fe9
Casey Burnett <burnett.casey@gmail.com>
2016-05-24 15:14:28 -0600

    Add comments explaining steps

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var nameData = document.querySelectorAll('.textInput');
 	console.log(nameData);
 
+// Loop through all elements of class and assign addEventListener
 for (var i = 0; i < nameData.length; i ++){
+	// IFFE function passing  i
 	(function(i){
 		nameData[i].addEventListener("click", function(){alertText(i)}, false);
 	})(i);
@@ -9,5 +11,6 @@ for (var i = 0; i < nameData.length; i ++){
 }
 
 
-function alertText(i){
-	alert("you did it, again." + " You have clicked item " + [i])}
+function alertText(j){
+	alert("you did it, again." + " You have clicked item " + [j + 1])
+}
