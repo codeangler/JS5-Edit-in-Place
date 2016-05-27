2d61b7add0827993ce8e72449667dfd3735dcda9
Casey Burnett <burnett.casey@gmail.com>
2016-05-26 18:10:35 -0600

    error continues

--- script.js
+++ script.js
@@ -18,20 +18,19 @@ for (var i = 0; i < nameData.length; i ++){
 
 function displayTextBox(j) {
 	j++;
-	var x = document.getElementById("content" + j);
+	var initialParagraph = document.getElementById("content" + j);
 	var y = document.createElement("input");
 		y.setAttribute("type", "text");
 		y.setAttribute("id", "dataInput" + j);
 		y.setAttribute("class", "textInput");
-		y.setAttribute("placeholder", x.innerHTML);
-		y.innerHTML = "replaced";
+		y.setAttribute("placeholder", initialParagraph.innerHTML);
 	// The append is an issued as the click is already passed... replace doens't give this erro x.appendChild(y);
 	// explaination http://stackoverflow.com/questions/843680/how-to-replace-dom-element-in-place-using-javascript
 	// Replace node with #info + j  with newly created  y  
-	x.parentNode.replaceChild(y, x); // double clicking doesn't produce an error
+	initialParagraph.parentNode.replaceChild(y, initialParagraph); // double clicking doesn't produce an error
 	
 	// Add new event handler for mouseleave on input
-	document.getElementById("dataInput" + j).addEventListener("mouseleave", function(){blurEvent(j)}, false);
+	document.getElementById("dataInput" + j).addEventListener("blur", function(){blurEvent(j)}, false);
 	
 }
 
